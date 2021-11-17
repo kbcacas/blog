c348dd227cef8324a1e2e4f90d609d46aa2f15f6
Kimcarl Cacas <kimcarlcacas@yahoo.com>
2021-11-17 20:06:52 +0800

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :articles
+
+  # get '/articles/new' => 'articles#new'
+  # post '/articles' => 'articles#create'
+
+  # get '/articles/:id/edit' => 'articles#edit'
+
+
+  # get '/articles/two' => 'articles#two'
+  # get '/articles' => 'articles#index'
+  # get '/articles/:id' => 'articles#show'
 end
